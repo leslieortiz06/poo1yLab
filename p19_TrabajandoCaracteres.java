3b9fae3159d50b3c32393810d3235a902ad0d2f5
leslieortiz06 <oleslienathali@gmail.com>
2024-08-21 07:43:16 -0600

    actividad 3

---
+++ p19_TrabajandoCaracteres.java
@@ -0,0 +1,34 @@
+// Trabajando con caracteres 
+
+public class p19_TrabajandoCaracteres {
+    public static void main(String[] args) {
+        Character c1 = 'A', c2 = 'B'; 
+        Character numero = '9';
+        Character arroba = '@';
+        Character pesos = 36;
+        Character corazon = '\u2665';
+        Character letraw = 119;
+        Character espacio = ' ';
+
+        System.out.print("\033[H\033[2J");System.out.flush();
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(numero);
+        System.out.println(arroba);
+        System.out.println(pesos);
+        System.out.println(corazon);
+        System.out.println(letraw);
+        System.out.println();
+
+        c1++;c2++;numero--;
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(numero);
+        System.out.println(Character.isLetter(c1));
+        System.out.println(Character.isDigit(numero));
+        System.out.println(Character.isAlphabetic(pesos));
+        System.out.println(Character.isSpaceChar(espacio));
+    }
+}
