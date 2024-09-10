e13b860d577ab6b3af43d785d45ea2c07611c77d
leslieortiz06 <oleslienathali@gmail.com>
2024-09-10 09:04:55 -0600

    primer examen

--- pe01_PrimerExamenParcial.java
+++ pe01_PrimerExamenParcial.java
@@ -1,3 +1,4 @@
+
 /**
  * pe01_PrimerExamenParcial
  */
@@ -6,53 +7,84 @@ import java.util.Scanner;
 public class pe01_PrimerExamenParcial {
     public static void main(String[] args) {
         String nombre;
-        int edad, totalDin, alumnosC, docentesC, trabajadoresC, rechazados, aceptados, cobro;
+        int edad, totalDin, alumnosC, docentesC, trabajadoresC, rechazados, aceptados, cobro, mujer, hombre;
+        int alumnos, docentes, trabajadores;
         char resp, sexo, tipoPar;
         Scanner obj = new Scanner(System.in);
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
 
-        totalDin = rechazados = aceptados = cobro = 0;
-        alumnosC = 40;
-        docentesC = 60;
-        trabajadoresC = 80; 
+        do {
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+    
+            totalDin = rechazados = aceptados = cobro = mujer = hombre = 0;
+            alumnos = docentes = trabajadores = alumnosC = docentesC = trabajadoresC = 0;
+    
+            System.out.print("Dame tu nombre: ");
+            nombre = obj.next();
+            System.out.print("Dame tu edad  : ");
+            edad = obj.nextInt();
+            System.out.print("Dame tu sexo [H] o [M]: ");
+            sexo = obj.next().charAt(0);
+            sexo = Character.toUpperCase(sexo);
+            System.out.print("Dame el tipo de participante [A]lumno  [D]ocente  [T]rabajador: ");
+            tipoPar = obj.next().charAt(0);
+            tipoPar = Character.toUpperCase(tipoPar);
+    
+            if (edad < 23) {
+                rechazados++;
+            } else {
+                System.out.printf(
+                        "\n%s bienvenid@ al curso, ingresas con una edad de %d, eres del sexo %s y tu tipo de participante es %s ",
+                        nombre, edad, sexo, tipoPar);
+                aceptados++;
+            }
+    
+            if (sexo == 'M') {
+                mujer++;
+            } else if (sexo == 'H') {
+                hombre++;
+            }
+    
+            if (tipoPar == 'A') {
+                alumnosC = 40;
+                alumnosC += cobro;
+                alumnos++;
+            } else if (tipoPar == 'D') {
+                docentesC = 60;
+                docentesC += cobro;
+                docentes++;
+            } else if (tipoPar == 'T') {
+                trabajadoresC = 80;
+                trabajadoresC += cobro;
+                trabajadores++;
+            }
+            totalDin = trabajadoresC + docentesC + alumnosC;
 
-        System.out.print("Dame tu nombre: ");
-        nombre = obj.nextLine();
-        System.out.print("Dame tu edad  : ");
-        edad = obj.nextInt();
-        System.out.print("Dame tu sexo [H] o [M]: ");
-        sexo = obj.next().charAt(0);
-        sexo = Character.toUpperCase(sexo);
-        System.out.print("Dame el tipo de participante [A]lumno  [D]ocente  [T]rabajador: ");
-        tipoPar = obj.next().charAt(0);
-        tipoPar = Character.toUpperCase(tipoPar);
+            System.out.print("\n\nQuieres inscribir a alguien más (S/N)?  ");
+            resp = Character.toUpperCase(obj.next().charAt(0));
+        } while (resp != 'N');
 
-        if (edad < 23) {
-            rechazados++;
-        } 
+        System.out.println("\n------------------------------------------");
+        System.out.printf("\nEl total de alumnos es de: %d", alumnos);
+        System.out.printf("\nEl total de docentes es de: %d", docentes);
+        System.out.printf("\nEl total de trabajadores es de: %d", trabajadores);
+        System.out.printf("\nEl total de hombres es de: %d", hombre);
+        System.out.printf("\nEl total de mujeres es de: %d", mujer);
+        System.out.printf("\nEl total de participantes es de: %d", aceptados);
+        System.out.printf("\nEl total de rechazados es de: %d", rechazados);
+        System.out.println("\n------------------------------------------");
+        System.out.printf("\nTotal de dinero recaudado de alumnos: %d", alumnosC);
+        System.out.printf("\nTotal de dinero recaudado de docentes: %d", docentesC);
+        System.out.printf("\nTotal de dinero recaudado de trabajadores: %d", trabajadoresC);
+        System.out.printf("\nTotal de dinero recaudado de todos los participantes es de: %d", totalDin);
+        
 
-        if (sexo == 'M') {
-            
-        } else {
-            
-        }
-
-        if (tipoPar == 'A') {
-            cobro += alumnosC;
-        } else if (tipoPar == 'D') {
-            cobro += docentesC;
-        } else if (tipoPar == 'T') {
-            cobro += trabajadoresC;
-        }
-
-
-        /*if (totalDin < 50) {
-            System.out.println("El evento concluye con ganancias BAJAS");
+        if (totalDin < 50) {
+        System.out.println("\n\nEl evento concluye con ganancias BAJAS");
         } else if (totalDin > 50 & totalDin <1000) {
-            System.out.println("El evento concluye con ganancias MODERADAS");
+        System.out.println("\n\nEl evento concluye con ganancias MODERADAS");
         } else if (totalDin > 1000) {
-            System.out.println("El evento concluye con BUENAS ganancias.");
-        }**/
+        System.out.println("\n\nEl evento concluye con BUENAS ganancias.");
+        }
     }
 }
