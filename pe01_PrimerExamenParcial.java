8652e01aef042e8984dd1b50137e9cd1ba9c9999
leslieortiz06 <oleslienathali@gmail.com>
2024-09-10 11:50:05 -0600

    primer examen

--- pe01_PrimerExamenParcial.java
+++ pe01_PrimerExamenParcial.java
@@ -1,4 +1,4 @@
-
+//Leslie Nathali Ortiz Sifuentes
 /**
  * pe01_PrimerExamenParcial
  */
@@ -17,10 +17,9 @@ public class pe01_PrimerExamenParcial {
 
         totalDin = rechazados = aceptados = cobro = mujer = hombre = participante = prom = sumaEdades = 0;
         alumnos = docentes = trabajadores = alumnosC = docentesC = trabajadoresC = 0;
-
         do {
             participante++;
-            System.out.printf("Participante: %d", participante);
+            System.out.printf("Participante: %d",participante); 
             System.out.print("\nDame tu nombre: ");
             nombre = obj.next();
             System.out.print("Dame tu edad  : ");
@@ -31,7 +30,7 @@ public class pe01_PrimerExamenParcial {
             System.out.print("Dame el tipo de participante [A]lumno  [D]ocente  [T]rabajador: ");
             tipoPar = obj.next().charAt(0);
             tipoPar = Character.toUpperCase(tipoPar);
-
+    
             if (edad < 23) {
                 rechazados++;
             } else {
@@ -45,26 +44,28 @@ public class pe01_PrimerExamenParcial {
                 } else if (sexo == 'H') {
                     hombre++;
                 }
+        
+                if (tipoPar == 'A') {
+                    alumnosC = 40;
+                    alumnosC += cobro;
+                    alumnos++;
+                } else if (tipoPar == 'D') {
+                    docentesC = 60;
+                    docentesC += cobro;
+                    docentes++;
+                } else if (tipoPar == 'T') {
+                    trabajadoresC = 80;
+                    trabajadoresC += cobro;
+                    trabajadores++;
+                }
+                totalDin = trabajadoresC + docentesC + alumnosC;
+                prom = sumaEdades/participante;
             }
-            
-            if (tipoPar == 'A') {
-                alumnosC = 40;
-                alumnosC += cobro;
-                alumnos++;
-            } else if (tipoPar == 'D') {
-                docentesC = 60;
-                docentesC += cobro;
-                docentes++;
-            } else if (tipoPar == 'T') {
-                trabajadoresC = 80;
-                trabajadoresC += cobro;
-                trabajadores++;
-            }
-            totalDin = trabajadoresC + docentesC + alumnosC;
-            prom = sumaEdades / participante;
+    
 
             System.out.print("\n\nQuieres inscribir a alguien más (S/N)?  ");
             resp = Character.toUpperCase(obj.next().charAt(0));
+            System.out.println("\n------------------------------------------");
         } while (resp != 'N');
 
         System.out.println("\n------------------------------------------");
@@ -74,20 +75,21 @@ public class pe01_PrimerExamenParcial {
         System.out.printf("\nEl total de hombres es de: %d", hombre);
         System.out.printf("\nEl total de mujeres es de: %d", mujer);
         System.out.printf("\nEl total de participantes es de: %d", aceptados);
-        System.out.printf("\nPromedio de la edad de todos los participantes: %d", prom);
+        System.out.printf("\nPromedio de la edad de todos los participantes: %d",prom);
         System.out.printf("\nEl total de rechazados es de: %d", rechazados);
         System.out.println("\n------------------------------------------");
         System.out.printf("\nTotal de dinero recaudado de alumnos: %d", alumnosC);
         System.out.printf("\nTotal de dinero recaudado de docentes: %d", docentesC);
         System.out.printf("\nTotal de dinero recaudado de trabajadores: %d", trabajadoresC);
         System.out.printf("\nTotal de dinero recaudado de todos los participantes es de: %d", totalDin);
+        
 
         if (totalDin < 50) {
-            System.out.println("\n\nEl evento concluye con ganancias BAJAS");
-        } else if (totalDin > 50 & totalDin < 1000) {
-            System.out.println("\n\nEl evento concluye con ganancias MODERADAS");
+        System.out.println("\n\nEl evento concluye con ganancias BAJAS");
+        } else if (totalDin > 50 & totalDin <1000) {
+        System.out.println("\n\nEl evento concluye con ganancias MODERADAS");
         } else if (totalDin > 1000) {
-            System.out.println("\n\nEl evento concluye con BUENAS ganancias.");
+        System.out.println("\n\nEl evento concluye con BUENAS ganancias.");
         }
     }
 }
