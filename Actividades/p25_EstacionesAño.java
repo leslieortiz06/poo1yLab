4ffbf3ac64fed8b99d84f129681e30edfcbe671b
leslieortiz06 <oleslienathali@gmail.com>
2024-09-25 09:45:34 -0600

    actividad 5

---
+++ Actividades/p25_EstacionesAño.java
@@ -0,0 +1,26 @@
+package Actividades;
+// Muestra la estación del año que corresponde al número introducido;
+
+import java.util.Scanner;
+
+public class p25_EstacionesAño {
+    public static void main(String[] args) {
+
+        System.out.print("\033[H\033[2J"); System.out.flush();
+
+        System.out.print("\nDame un número entre 1 y4 y te dire q estacion del año es: ");
+        int n = new Scanner(System.in).nextInt();
+
+        if (n>=1 && n<=4) {
+            System.out.println("Chido");
+            if(n==1) System.out.println("\nPrimavera");
+            if(n==2) System.out.println("\nVerano");
+            if(n==3) System.out.println("\nOtoño");
+            if(n==4) System.out.println("\nInvierno");
+        }
+        else{
+            System.out.println("\nEsas estaciones del año no existen !!! ");
+        }
+        System.out.println("\nVamonos al comedor...");
+    }   
+}
