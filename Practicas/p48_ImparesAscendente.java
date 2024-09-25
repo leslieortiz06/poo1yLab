655bd753b41a9f40d01705f4bbe6ccb0c4587caf
leslieortiz06 <oleslienathali@gmail.com>
2024-09-25 09:52:56 -0600

    practica 3

---
+++ Practicas/p48_ImparesAscendente.java
@@ -0,0 +1,42 @@
+package Practicas;
+import java.util.Scanner;
+
+/**
+ * p48_ImparesAscendente
+ */
+public class p48_ImparesAscendente {
+
+    public static void main(String[] args) {
+        int c, n, s, ci;
+        double p;
+        char resp;
+        Scanner obj = new Scanner(System.in);
+
+        do {
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+
+            c = 1;
+            n = s = ci = 0;
+            System.out.println("Imprime impares de forma ascendente de 1 hasta n\n");
+            System.out.print("Hasta donde deseas los impares?: ");
+            n = obj.nextInt();
+
+            while (c <= n) {
+                System.out.printf("%d ", c);
+                s += c;
+                ci++;
+                c += 2;
+
+            }
+            p = ci > 0 ? (double) s / ci : 0;
+            System.out.printf("\nLa suma de los impares hasta %d es %d ", n, s);
+            System.out.printf("\nEl promedio de los impares es %.2f \n", p);
+
+            System.out.print("Deseas continuar (S/N)?: ");
+            resp = Character.toUpperCase(obj.next().charAt(0));
+
+        } while (resp != 'N');
+        System.out.println("\nProceso terminado...");
+    }
+}
---
+++ Practicas/p50_ConversionTemperaturas.java
@@ -0,0 +1,35 @@
+package Practicas;
+import java.util.Scanner;
+
+public class p50_ConversionTemperaturas {
+    public static void main(String[] args) {
+        double tempInicial, tempFinal, celsius, fahrenheit;
+        char resp;
+        Scanner obj = new Scanner(System.in);
+
+        do {
+            System.out.print("\033[H\033[2J"); System.out.flush();
+
+            System.out.println("Convertir de grados Centigrados a Farenheit\n");
+            System.out.print("Introduce la temperatura inicial en grados Celsius: ");
+            tempInicial = obj.nextDouble();
+
+            System.out.print("Introduce la temperatura final en grados Celsius: ");
+            tempFinal = obj.nextDouble();
+
+            System.out.println("\nTemperaturas en grados Fahrenheit: \n");
+
+            celsius = tempInicial;
+            while (celsius <= tempFinal) {
+                fahrenheit = (celsius * 9 / 5) + 32;
+                System.out.printf("%.2f °C   =   %.2f °F%n", celsius, fahrenheit);
+                celsius ++;
+            }
+            
+            System.out.print("\nDeseas continuar (S/N)? ");
+            resp = Character.toUpperCase(obj.next().charAt(0));
+
+        } while (resp != 'N');
+        System.out.println("\nProceso terminado");
+    }
+}
