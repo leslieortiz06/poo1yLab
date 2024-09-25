33e3d53bc704decee6f556cbdede8d0326d6e7c1
leslieortiz06 <oleslienathali@gmail.com>
2024-09-25 09:45:05 -0600

    actividad 4

---
+++ Actividades/p22_VerificaSuma.java
@@ -0,0 +1,28 @@
+package Actividades;
+// Verifica si la suma de dos numeros es igual a un tercero
+
+import java.util.Scanner;
+
+public class p22_VerificaSuma {
+    public static void main(String[] args) {
+        float n1,n2,n3;
+        Scanner obj = new Scanner(System.in);
+
+        System.out.print("\033[H\033[2J"); System.out.flush();
+
+        System.out.println("Verificar si la suma de dos numeros es igual a un tercero\n");
+        System.out.print("Dame el primer numero : "); n1 = obj.nextFloat();
+        System.out.print("Dame el seguno numero : "); n2 = obj.nextFloat();
+        System.out.print("Dame el tercer numero : "); n3 = obj.nextFloat();
+
+        if( n1+n2 == n3 ){
+         System.out.println("\nSon iguales");
+         System.out.println(n1 + " + " + n2 + " = " + n3);
+        }
+        else{
+         System.out.println("\nSon distintos");
+         System.out.println(n1 + " + " + n2 + " != " + n3);
+        }
+        System.out.println("\nGracias por utilizar este programa");
+    }
+}
