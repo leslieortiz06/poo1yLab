8291f89da435c3f15fb7334d94bdc95aaebb3d6c
leslieortiz06 <oleslienathali@gmail.com>
2024-09-25 09:46:05 -0600

    actividad 6

---
+++ Actividades/p29_Calculadora.java
@@ -0,0 +1,45 @@
+package Actividades;
+// Efectua operaciones matematicas basicas sobre dos números 
+
+import java.util.Scanner;
+
+public class p29_Calculadora {
+    public static void main(String[] args) {
+        double n1, n2;
+        char op;
+        Scanner obj = new Scanner(System.in);
+
+        System.out.print("\033[H\033[2J");System.out.flush();
+
+        System.out.println("\nEfectuando operaciones matematicas basicas sobre 2 números");
+        System.out.print("Dame numero 1: ");
+        n1 = obj.nextFloat();
+        System.out.print("Dame numero 2: ");
+        n2 = obj.nextFloat();
+        System.out.print("Operacion + - * / ^ : ");
+        op = obj.next().charAt(0);
+
+        switch (op) {
+            case '+':
+                System.out.printf("%.2f   +    %.2f = %.2f", n1, n2, n1 + n2);
+                break;
+            case '-':
+                System.out.printf("%.2f   -    %.2f = %.2f", n1, n2, n1 - n2);
+                break;
+            case '*':
+                System.out.printf("%.2f   *    %.2f = %.2f", n1, n2, n1 * n2);
+                break;
+            case '/':
+                System.out.printf("%.2f   /    %.2f = %.2f", n1, n2, n1 / n2);
+                break;
+            case '^':
+                System.out.printf("%.2f   ^    %.2f = %.2f", n1, n2, Math.pow(n1, n2));
+                break;
+
+            default:
+                System.out.println("\nEsa operacion no esta implementada, tus chilaquiles estan en riesgo\n");
+                break;
+        }
+        System.out.println("\nYa te estas ganando tus chilaquiles...");
+    }
+}
